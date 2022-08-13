7fd9a70b02f0aa47a6402fc93bd37a9eb82a6ae4
ciyeer <ciyeer@outlook.com>
2022-08-14 02:56:28 +0800

    update

--- admin/adminwidget.cpp
+++ admin/adminwidget.cpp
@@ -1,13 +1,18 @@
 #include "adminwidget.h"
 #include "ui_adminwidget.h"
+#include <QMessageBox>
+#include <QSqlRecord>
+#include <QDebug>
+#include <QTableWidgetItem>
+#include <QSqlError>
 
 AdminWidget::AdminWidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::AdminWidget){
     ui->setupUi(this);
 
-    // connection();
-    setUpTableView();
+    connection();
+    setUpTableView();    
 }
 
 void AdminWidget::connection(){
@@ -17,10 +22,10 @@ void AdminWidget::connection(){
             this, &AdminWidget::slotDeleteUser);
     connect(ui->modifyBtn, &QPushButton::clicked,
             this, &AdminWidget::slotModifyUserInfo);
-    connect(ui->serachuser, &QPushButton::clicked,
-            this, &AdminWidget::slotQueryUser);
-    connect(ui->flushBtn, &QPushButton::clicked,
-            this, &AdminWidget::slotClearUserInfo);
+//    connect(ui->serachuser, &QPushButton::clicked,
+//            this, &AdminWidget::slotQueryUser);
+//    connect(ui->flushBtn, &QPushButton::clicked,
+//            this, &AdminWidget::slotClearUserInfo);
     connect(ui->exitBtn, &QPushButton::clicked,
             this, &AdminWidget::slotReturnLoginWidget);
 }
@@ -52,9 +57,66 @@ void AdminWidget::setUpTableView(){
     // connect(ui->user_tableView->horizontalHeader(), SIGNAL(sectionClicked(int)), this, SLOT(sortByColumn(int)));
 }
 
+// QTableView修改一行的保存操作
+void AdminWidget::slotFlushUserInfo(){
+    QSqlTableModel *pMode = dynamic_cast<QSqlTableModel *>(ui->user_tableView->model());
+    pMode->database().transaction();        // 开始事务操作
+    if (pMode->submitAll())                 // 提交所有被修改的数据到数据库中
+    {
+        pMode->database().commit();         // 提交成功，事务将真正修改数据库数据
+    } else {
+        pMode->database().rollback();       // 提交失败，事务回滚
+        QMessageBox::warning(this, tr("tableModel"),tr("数据库错误: %1").arg(pMode->lastError().text()));
+    }
+    pMode->revertAll(); // 撤销修改
+}
+
+// QTableView新增一行的操作
 void AdminWidget::slotAddUser(){
-    AddUserInfoDialog addDlg;
-    addDlg.exec();
+    QSqlTableModel *pMode = dynamic_cast<QSqlTableModel *>(ui->user_tableView->model());
+    QSqlRecord record = pMode->record();
+    pMode->setRecord(pMode->rowCount(), record);
+    int number;
+    // 从1开始遍历，遇到相同的，自增再遍历，直到没有相同的作为number插入
+    // 防止唯一主键重复导致提交失败，因为提交失败tableview还是会更新修改后的（视图）
+    for(number = 1; ; number++){
+        bool bFlag = false;
+        for(int index = 0; index < pMode->rowCount(); index++) {
+            if(pMode->index(index, 0).data().toInt() == number) {
+                bFlag = true;
+                break;
+            }
+        }
+        if(!bFlag) {
+            break;
+        }
+    }
+
+    pMode->insertRecord(pMode->rowCount(), record);
+    // 每次手动提交，都会重新刷新tableView，保持mode和tableView一致
+    pMode->submitAll();
+}
+
+// 删除用户
+void AdminWidget::slotDeleteUser(){
+    //设置表格的单元为只读属性，即不能编辑
+    ui->user_tableView->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    //如果你用在QTableView中使用右键菜单，需启用该属性
+    ui->user_tableView->setContextMenuPolicy(Qt::CustomContextMenu);
+    QSqlTableModel *pMode = dynamic_cast<QSqlTableModel *>(ui->user_tableView->model());
+    // 当QSqlTableModel::EditStrategy 选择 非QSqlTableModel::OnManualSubmit 时,
+    // 每次删除都可删除掉model，但是tableview那一列为空，一直在，只好设置为 QSqlTableModel::OnManualSubmit
+    pMode->removeRow(ui->user_tableView->currentIndex().row());
+    // 每次手动提交，都会重新刷新tableView，保持mode和tableView一致
+    pMode->submitAll();
+}
+
+void AdminWidget::sortByColumn(int col){
+    QSqlTableModel *pMode = dynamic_cast<QSqlTableModel *>(ui->user_tableView->model());
+    bool ascending = (ui->user_tableView->horizontalHeader()->sortIndicatorSection()==col
+                      && ui->user_tableView->horizontalHeader()->sortIndicatorOrder()==Qt::DescendingOrder);
+    Qt::SortOrder order = ascending ? Qt::AscendingOrder : Qt::DescendingOrder;
+    pMode->sort(col, order);
 }
 
 void AdminWidget::slotQueryUser(){
@@ -62,20 +124,16 @@ void AdminWidget::slotQueryUser(){
     queryDlg.exec();
 }
 
-void AdminWidget::slotDeleteUser(){
-    deleteUserInfoDialog deleteDlg;
-    deleteDlg.exec();
-}
-
 void AdminWidget::slotModifyUserInfo(){
-    ModifyUserInfoDialog modifyDlg;
-    modifyDlg.exec();
+    // 将表格的编辑模式打开
+    ui->user_tableView->setEditTriggers(QAbstractItemView::DoubleClicked);
 }
 
 void AdminWidget::slotClearUserInfo(){
     ClearUserInfoDialog clearDlg;
     clearDlg.exec();
 }
+
 
 void AdminWidget::slotReturnLoginWidget(){
     emit display(0);
@@ -84,6 +142,7 @@ void AdminWidget::slotReturnLoginWidget(){
 AdminWidget::~AdminWidget(){
     delete ui;
 }
+
 
 
 
--- admin/adminwidget.h
+++ admin/adminwidget.h
@@ -39,8 +39,12 @@ public slots:
     void slotModifyUserInfo();
     // 清空用户信息
     void slotClearUserInfo();
+    // 刷新(更新)信息
+    void slotFlushUserInfo();
     // 返回登录界面
     void slotReturnLoginWidget();
+    // QTableView点击列头的排序操作(初始化时，开启了列头clicked和关联信号槽)
+    void sortByColumn(int col);
 
 private:
     Ui::AdminWidget *ui;
